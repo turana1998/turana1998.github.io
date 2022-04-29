36bcb0d4392b8a7a8008476c90275992ced29560
turana1998 <turana221298@gmail.com>
2022-04-29 17:17:47 +0400

    jj

--- mastero/js/main.js
+++ mastero/js/main.js
@@ -46,13 +46,6 @@ $("#testimonials .regular").slick({
           }
         },
         {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
           breakpoint: 480,
           settings: {
             slidesToShow: 1,
