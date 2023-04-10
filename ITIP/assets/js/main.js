3db9366949bf7f63408bf6d4dfb51fe23ae888f2
turana1998 <turana221298@gmail.com>
2023-04-10 17:23:04 +0400

    data table responsive

--- ITIP/assets/js/main.js
+++ ITIP/assets/js/main.js
@@ -154,7 +154,6 @@
 $(document).ready(function () {
   //Only needed for the filename of export files.
   //Normally set in the title tag of your page.
-  document.title = 'Simple DataTable';
   // DataTable initialisation
   $('#example').DataTable(
       {
